f4109814eb4f4c270a09d8fa973350e2fbf6c059
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-17 12:43:15 -0800

    added fake data

---
+++ src/utils/dummyData.js
@@ -0,0 +1,158 @@
+export const dummyData = [
+    {
+        id: 1,
+        user: 'Bob',
+        password: '5678',
+        image: '',
+        city: 'Fargo',
+        state: 'ND',
+        tools: [
+            'snowblower', 
+            'woodchipper',
+            'chainsaw'
+        ]
+    },
+    {
+        id: 2,
+        user: 'Joe',
+        password: '1234',
+        image: '',
+        city: 'Casper',
+        state: 'WY',
+        tools: [
+            'axe', 
+            'leafblower',
+            'sledgehammer'
+        ]  
+    },
+    {
+        id: 3,
+        user: 'Bill',
+        password: 'asdf',
+        image: '',
+        city: 'Wilmington',
+        state: 'DE',
+        tools: [
+            'air compressor', 
+            'framing gun',
+            'finish gun'
+        ]
+    },
+    {
+        id: 4,
+        user: 'John',
+        password: 'jkl;',
+        image: '',
+        city: 'Austin',
+        state: 'TX',
+        tools: [
+            'lawnmower', 
+            'seed spreader',
+            'rake'
+        ]  
+    },
+    {
+        id: 5,
+        user: 'Paul',
+        password: 'good',
+        image: '',
+        city: 'Memphis',
+        state: 'TN',
+        tools: [
+            'pneumatic wood splitter', 
+            'torque wrench',
+            '50 foot tape measure'
+        ]
+    },
+    {
+        id: 6,
+        user: 'George',
+        password: 'better',
+        image: '',
+        city: 'Ann Arbor',
+        state: 'MI',
+        tools: [
+            'winch', 
+            'ratchet set',
+            'arc welder'
+        ]  
+    },
+    {
+        id: 7,
+        user: 'Ringo',
+        password: 'best',
+        image: '',
+        city: 'Buffalo',
+        state: 'NY',
+        tools: [
+            'blow torch', 
+            'flamethrower',
+            'hacksaw'
+        ]
+    },
+    {
+        id: 8,
+        user: 'Jim',
+        password: 'blob#',
+        image: '',
+        city: 'St Louis',
+        state: 'MO',
+        tools: [
+            'table saw', 
+            'mitre saw',
+            'hand saw'
+        ]  
+    },
+    {
+        id: 9,
+        user: 'Steve',
+        password: 'hero42',
+        image: '',
+        city: 'Seattle',
+        state: 'WA',
+        tools: [
+            'cement mixer', 
+            'masonry toolset',
+            'impact driver'
+        ]
+    },
+    {
+        id: 10,
+        user: 'Adam',
+        password: 'next9',
+        image: '',
+        city: 'Phoenix',
+        state: 'AZ',
+        tools: [
+            'chimney sweep kit', 
+            'volt meter',
+            '20 foot ladder'
+        ]  
+    },
+    {
+        id: 11,
+        user: 'Suzie',
+        password: 'believe!',
+        image: '',
+        city: 'Anchorage',
+        state: 'AK',
+        tools: [
+            'sluice box', 
+            'pick axe',
+            'shovel'
+        ]
+    },
+    {
+        id: 12,
+        user: 'Jenny',
+        password: '9876',
+        image: '',
+        city: 'Billings',
+        state: 'MT',
+        tools: [
+            'leather punch kit', 
+            'sewing machine',
+            'pottery wheel'
+        ]  
+    }
+]
