57931b3d49b2c104947f7a04e95eec5192fdc5fa
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-18 07:12:56 -0800

    changed dummy data test a bit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import PracticeCard from './components/PracticeCard';
 function App() {
     return (
         <div className="App">
-            {/* This is a feasiblity study to verify that dummy data actually works good. */}
+            {/* This is a feasiblity test to verify that dummy data actually works good. */}
             <h1>Feasibility Test For Dummy Data</h1>
             <div className='card-list'>
                 {dummyUsers.map(user => {
