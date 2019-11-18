84035571135e746149b3a691a4b37818d24b0254
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-18 14:58:20 -0800

    added single user dummy data and got example render on App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Login from './components/Sam/Login';
 import SignUp from './components/Sam/SignUp';
 import './App.css';
 
+import { dummySingleUser } from './utils/dummySingleUser';
+
 function App() {
     return (
         <div className="App">
             <h1>App Title</h1>
-            <Route exact path="/" component={Login} /> 
+            <Route exact path="/" component={Login} />
             <Route path="/sign-up" component={SignUp} />
+            <h2>{dummySingleUser.user.username}</h2>
+            <h4>{dummySingleUser.user.location}</h4>
+            {dummySingleUser.tools.map(tool => {
+                return <div key={tool.id}>
+                    <p>Name: {tool.name}</p>
+                    <p>Price: {tool.price}</p>
+                </div>
+            })}
+
             {/* <PrivateRoute path="/tool-list" component={} />
             <PrivateRoute path="/user/:id" component={} /> */}
         </div>
