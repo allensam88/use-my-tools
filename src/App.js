e2b476f584580d366df6c7f4f2c05749792550b1
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-18 13:51:14 -0800

    got sign-in and sign-up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute';
+import Login from './components/Sam/Login';
+import SignUp from './components/Sam/SignUp';
 import './App.css';
-import { dummyUsers } from './utils/dummyUsers';
-import PracticeCard from './components/PracticeCard';
 
 function App() {
     return (
         <div className="App">
-        
             <h1>App Title</h1>
-
+            <Route exact path="/" component={Login} /> 
+            <Route path="/sign-up" component={SignUp} />
+            {/* <PrivateRoute path="/tool-list" component={} />
+            <PrivateRoute path="/user/:id" component={} /> */}
         </div>
     );
 }
