533e770d4a38b297ecc025d892f59a0d60156ad8
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-18 18:32:45 -0800

    changed a few things to make deployment happen

--- src/App.js
+++ src/App.js
@@ -4,25 +4,22 @@ import PrivateRoute from './utils/PrivateRoute';
 import Login from './components/Sam/Login';
 import SignUp from './components/Sam/SignUp';
 import './App.css';
-import { dummyUsers } from './utils/dummyUsers';
 import ToolList from './components/Aaron/ToolList';
-import { dummySingleUser } from './utils/dummySingleUser';
+import Starter from './components/Kai/starter';
 
 function App() {
     return (
         <div className="App">
             <h1>Use My Tools!!!</h1>
+            
             <Route exact path="/" component={Login} />
+            
             <Route path="/sign-up" component={SignUp} />
-            <h2>{dummySingleUser.user.username}</h2>
-            <h4>{dummySingleUser.user.location}</h4>
-            {dummySingleUser.tools.map(tool => {
-                return <div key={tool.id}>
-                    <p>Name: {tool.name}</p>
-                    <p>Price: {tool.price}</p>
-                </div>
-            })}
+            
             <PrivateRoute path="/tools" component={ToolList} />
+            
+            {/* This will be dynamic :id */}
+            <PrivateRoute path="/user/" component={Starter} />
         </div>
     );
 }
