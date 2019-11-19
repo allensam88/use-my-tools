1eda6f7afd2209ad66c35b55bb1dfd0714c42c60
kai-haskell <me@kaihaskell.com>
2019-11-19 17:15:27 -0500

    finished nav

--- src/components/Kai/navBar.js
+++ src/components/Kai/navBar.js
@@ -3,20 +3,19 @@ import "../../Components.css";
 import { Link } from "react-router-dom";
 import logo from "../../img/Wht-Logo.png";
 
-
 const NavBar = () => {
   return (
     <nav className="navbar navbar-custom">
       <img src={logo} alt={"logo"} />
       <ul className="nav">
         <li className="nav-item">
-          <Link to="/toolList" className="nav-link">Link</Link>
+          <Link className="nav-link" to="/tools" className="nav-link">
+            Tools
+          </Link>
         </li>
         <li className="nav-item">
-          <Link to="/profile">
-            <a className="nav-link" href="#">
-              Profile
-            </a>
+          <Link className="nav-link" to="/profile">
+            Profile
           </Link>
         </li>
       </ul>
