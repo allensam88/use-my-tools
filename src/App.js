abdd901db7a858402de3b1dfa1f77f9c8609f583
kai-haskell <me@kaihaskell.com>
2019-11-19 16:47:50 -0500

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import PrivateRoute from './utils/PrivateRoute';
-import Login from './components/Sam/Login';
-import SignUp from './components/Sam/SignUp';
-import './App.css';
-import ToolList from './components/Aaron/ToolList';
+import React from "react";
+import { Route } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import Login from "./components/Sam/Login";
+import SignUp from "./components/Sam/SignUp";
+import "./Components.css";
+import ToolList from "./components/Aaron/ToolList";
 import Profile from "./components/Kai/profile";
 import NavBar from "./components/Kai/navBar";
-
+import Footer from "./components/Kai/footer";
 function App() {
-    return (
-        <div className="App">
-            <h1>Use My Tools!!!</h1>
-            <NavBar />
-            <Route exact path="/" component={Login} />
-            
-            <Route path="/sign-up" component={SignUp} />
-            
-            <PrivateRoute path="/tools" component={ToolList} />
-            
-            <PrivateRoute path="/user/:id" component={Profile} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <NavBar />
+      <Route exact path="/" component={Login} />
+
+      <Route path="/sign-up" component={SignUp} />
+
+      <PrivateRoute path="/tools" component={ToolList} />
+
+      <PrivateRoute path="/user/:id" component={Profile} />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
