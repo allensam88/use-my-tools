d1730de381b7f25bf5ca823fb1f4b17a7950c9f9
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-20 07:50:42 -0800

    add tool working, started update user

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import './App.css';
 import NavBar from "./components/Kai/navBar";
 import ToolList from "./components/Aaron/ToolList";
 import Profile from "./components/Kai/profile";
+import UpdateUser from "./components/Sam/UpdateUser";
+import AddTool from "./components/Aaron/AddTool";
 import UpdateTool from "./components/Sam/UpdateTool";
 import DeleteTool from './components/Sam/DeleteTool';
 import Footer from "./components/Kai/footer";
@@ -23,6 +25,10 @@ function App() {
       <PrivateRoute path="/tools" component={ToolList} />
 
       <PrivateRoute path="/user/:id" component={Profile} />
+
+      <PrivateRoute path="/update-user/:id" component={UpdateUser} />
+
+      <PrivateRoute path="/add-tool/" component={AddTool} />
 
       <PrivateRoute path="/update-tool/:id" component={UpdateTool} />
 
