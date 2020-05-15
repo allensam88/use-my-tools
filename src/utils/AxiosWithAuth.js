ce2e3623472f2dcb79ce9a629665f1c1a818f3c5
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-05-15 16:22:51 -0700

    added dotenv

--- src/utils/AxiosWithAuth.js
+++ src/utils/AxiosWithAuth.js
@@ -4,10 +4,21 @@ const AxiosWithAuth = () => {
 	const token = localStorage.getItem('token');
 
 	return axios.create({
-		baseURL: 'http://localhost:5000',
+		baseURL: (function () {
+			switch (process.env.REACT_APP_ENV) {
+				case "development":
+					return process.env.REACT_APP_LOCAL_HOST;
+				case "production":
+					return process.env.REACT_APP_PRODUCTION_URL;
+				default:
+					return process.env.REACT_APP_LOCAL_HOST;
+			}
+		})(),
 		headers: {
-			Authorization: token
-		}
+			// "Authorization": token,
+			"Authorization": token,
+			"Content-Type": "application/json",
+		},
 	});
 };
 
