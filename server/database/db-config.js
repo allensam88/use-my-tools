ebe7828995597d5eebf0403efb78630c3e106962
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-05-15 18:15:18 -0700

    removed server

--- server/database/db-config.js
+++
@@ -1,7 +0,0 @@
-const knex = require('knex');
-
-const knexConfig = require('../knexfile');
-
-const dbEnv = process.env.DB_ENV || 'development'
-
-module.exports = knex(knexConfig[dbEnv]);
--- server/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const server = require('./api/server');
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-	console.log(`\n=== Server listening on port ${PORT} ===\n`)
-})
