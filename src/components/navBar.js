c92832f47b13e6fa99cae2a2fc5ddea8088cab17
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-05-25 13:39:03 -0700

    added logout link

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,29 +1,33 @@
 import React from "react";
 import "../Components.css";
 import { Link } from "react-router-dom";
+import Logout from "./Logout";
 import logo from "../img/Wht-Logo.png";
 
 const NavBar = () => {
-  const userId = localStorage.getItem("userId");
-  return (
-    <nav className="navbar navbar-custom">
-      <Link to="/">
-        <img src={logo} alt={"logo"} />
-      </Link>
-      <ul className="nav">
-        <li className="nav-item">
-          <Link className="nav-link" to="/tools">
-            Tools
+	const userId = localStorage.getItem("userId");
+	return (
+		<nav className="navbar navbar-custom">
+			<Link to="/">
+				<img src={logo} alt={"logo"} />
+			</Link>
+			<ul className="nav">
+				<li className="nav-item">
+					<Link className="nav-link" to="/tools">
+						Tools
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to={`/user/${userId}`}>
-            Profile
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to={`/user/${userId}`}>
+						Profile
           </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+				</li>
+				<li className="nav-item">
+					<Logout />
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default NavBar;
